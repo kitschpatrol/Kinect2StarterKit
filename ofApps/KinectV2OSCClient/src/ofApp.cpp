4edb3eb91fc9f1cc8f6432890299b2497e26abad
kitschpatrol <ermika@gmail.com>
2014-06-21 14:01:53 -0400

    Basic OSC logging in the client.

--- ofApps/KinectV2OSCClient/src/ofApp.cpp
+++ ofApps/KinectV2OSCClient/src/ofApp.cpp
@@ -2,11 +2,42 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
+	ofSetLogLevel(OF_LOG_VERBOSE);
+
+	const int port = 30001;
+	oscReceiver.setup(port);
+	ofLog(OF_LOG_VERBOSE, "Receiving OSC on port: " + ofToString(port));
+
 
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
+	ofxOscMessage m;
+	while (oscReceiver.getNextMessage(&m)) {
+		ofLog(OF_LOG_VERBOSE, m.getAddress());
+
+		for (int i = 0; i < m.getNumArgs(); i++) {
+			ofxOscArgType argType = m.getArgType(i);
+
+			switch (argType)
+			{
+			case OFXOSC_TYPE_FLOAT:
+				ofLog(OF_LOG_VERBOSE, ofToString(i) + ": " + ofToString(m.getArgAsFloat(i)));
+				break;
+			case OFXOSC_TYPE_INT32:
+				ofLog(OF_LOG_VERBOSE, ofToString(i) + ": " + ofToString(m.getArgAsInt32(i)));
+				break;
+			case OFXOSC_TYPE_STRING:
+				ofLog(OF_LOG_VERBOSE, ofToString(i) + ": " + ofToString(m.getArgAsString(i)));
+				break;
+			default:
+				ofLog(OF_LOG_VERBOSE, ofToString(i) + ": " + "unhandled");
+				break;
+			}
+
+		}
+	}
 
 }
 
--- ofApps/KinectV2OSCClient/src/ofApp.h
+++ ofApps/KinectV2OSCClient/src/ofApp.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "ofMain.h"
+#include "ofxOsc.h"
 
 class ofApp : public ofBaseApp{
 
@@ -18,5 +19,6 @@ class ofApp : public ofBaseApp{
 		void windowResized(int w, int h);
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
-		
+
+		ofxOscReceiver oscReceiver;		
 };
